89f3848f0c34f461c5c8f32016672f84c389f3d4
Dev_Azi <azi26.dev@gmail.com>
2025-04-07 11:17:32 +0800

    first commit

---
+++ database/migrations/2025_04_07_024722_add_foreign_key_to_employees.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->foreign('job_id')->references('id')->on('jobstbl')->onDelete('cascade');
+            $table->foreign('manager_id')->references('id')->on('employees')->onDelete('cascade');
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+        });
+    }
+};
