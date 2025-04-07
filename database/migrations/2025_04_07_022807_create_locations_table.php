0d064ea5ae8a60af9f33da962df445643d21bf33
Dev_Azi <azi26.dev@gmail.com>
2025-04-07 11:23:44 +0800

    first commit

--- database/migrations/2025_04_07_022807_create_locations_table.php
+++ database/migrations/2025_04_07_022807_create_locations_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('state_province');
             $table->unsignedBigInteger('country_id');
 
-          //  $table->foreign('location_id')->references('id')->on('countries')->onDelete('cascade');
             $table->timestamps();
         });
     }
